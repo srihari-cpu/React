4ed902a0c9eafc3dc5fde0501d977ef0466f7273
Srihari <srihariyadav1343@gmail.com>
2025-03-15 11:14:42 +0530

    calculated project completed full-fledged

--- projects/5-calculate-version-two/src/App.jsx
+++ projects/5-calculate-version-two/src/App.jsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import styles from "./App.module.css";
 import ButtonContainer from "./Components/ButtonsContainer";
 import Display from "./Components/Display";
 function App() {
+  let [calValue, setCalValue] = useState("");
+  const onButtonClick = (buttonName) => {
+    if (buttonName === "C") {
+      setCalValue("");
+    } else if (buttonName === "=") {
+      const result = eval(calValue);
+      setCalValue(result);
+    } else {
+      let newDisplayValue = calValue + buttonName;
+      setCalValue(newDisplayValue);
+    }
+  };
   return (
     <div className={styles.calculator}>
-      <Display />
-      <ButtonContainer />
+      <Display displayValue={calValue} />
+      <ButtonContainer onButtonClick={onButtonClick} />
     </div>
   );
 }
